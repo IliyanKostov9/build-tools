83eb2826ffa7a79e4ae9725137a49d934b9d51a0
iliyan-kostov <iliyan.kostov@email.ikostov.org>
2024-10-13 15:44:08 +0300

    feat: added CMake

---
+++ CMake/java/build/CMakeFiles/JavaHelloJar.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "/home/ikostov2/repos/personal/sandbox/java/simple-java-maven-app/target/JavaHelloJar-1.0.0.jar"
+  "CMakeFiles/JavaHelloJar"
+  "CMakeFiles/JavaHelloJar.dir/java_class_filelist"
+  "CMakeFiles/JavaHelloJar.dir/java_compiled_JavaHelloJar"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang )
+  include(CMakeFiles/JavaHelloJar.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ CMake/java/build/CMakeFiles/JavaHelloJar.dir/compiler_depend.make
@@ -0,0 +1,2 @@
+# Empty custom commands generated dependencies file for JavaHelloJar.
+# This may be replaced when dependencies are built.
